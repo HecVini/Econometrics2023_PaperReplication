c8d7c1689ddc99b0d3440f15f4bf3bf9b2a43304
guilhermecota <146790529+guilhermecota@users.noreply.github.com>
2023-11-07 00:50:47 -0300

    Delete pasta directory

--- pasta/a0Master.R
+++
@@ -1,8 +0,0 @@
-#Estima um modelo apenas com os dados de Chi e estima outro com os dados de ambas as regiões e suas correlações
-
-#setwd('')
-WP<-readRDS('MetroAreasData.rds')
-
-a2<-a2Estimate(WP$Chi$YIk,WP$Chi$VIk,WP$Chi$PIk,WP$Chi$Pop,WP$Chi$Corr)
-
-a3<-a3Estimate(WP$Chi$YIk,WP$Chi$VIk,WP$Chi$PIk,WP$Chi$Pop,WP$NYC$YIk,WP$NYC$VIk,WP$NYC$PIk,WP$NYC$Pop,WP$Agr$Corr)
